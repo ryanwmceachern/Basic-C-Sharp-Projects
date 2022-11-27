55e143a0776d28e1766524f3635e529ecdc27490
ryanwmceachern <105886833+ryanwmceachern@users.noreply.github.com>
2022-11-27 11:41:07 -0400

    Add files via upload

--- methodsclass/methodsclass/Class1.cs
+++ methodsclass/methodsclass/Class1.cs
@@ -8,11 +8,11 @@ namespace methodsclass
 {
     internal class mathclass
     {
-        public void math(double num1, double num2)
+        public void math(int num1, int num2)
         {
-            double Total = num1 + 10;
-            Console.WriteLine("\n{0} + 10 = " + Total + ".", num1);
-            Console.WriteLine("\n{0} is not included in the operation", num2);
+            int Total = num1 + 10;
+            Console.WriteLine("\nyour number {0} plus ten = " + Total + ".", num1);
+            Console.WriteLine("\nyour number {0} is not included in the operation", num2);
             return;
         }
     }
