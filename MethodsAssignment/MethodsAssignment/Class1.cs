f60619a076adb095881828f89f410ef5fab62ba5
ryanwmceachern <105886833+ryanwmceachern@users.noreply.github.com>
2022-11-27 14:30:31 -0400

    Add files via upload

--- MethodsAssignment/MethodsAssignment/Class1.cs
+++ MethodsAssignment/MethodsAssignment/Class1.cs
@@ -11,7 +11,7 @@ namespace MethodsAssignment
     internal class Class1
     {
         
-        public double math(double num1, [Optional] double num2)
+        public double math(int num1, [Optional] int num2)
         {
             double totalAmount = num1 + num2 + 25;
             return totalAmount;
--- MethodsAssignment/MethodsAssignment/Program.cs
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -12,9 +12,9 @@ namespace MethodsAssignment
         {
             //Asking the user to enter two integers one mandatory one optional.
             Console.WriteLine("Enter an integer.");
-            double a = Convert.ToDouble(Console.ReadLine());
+            int a = (Console.ReadLine());
             Console.WriteLine("\nYou now have the option to enter a second integer to the equation. (Note this integer is optional.)");
-            string b = Console.ReadLine();
+            int b = (Console.ReadLine());
             Console.WriteLine("\nWe will now add your Integer(s).");
             
 
@@ -22,18 +22,18 @@ namespace MethodsAssignment
             
             Class1 method = new Class1();
             //A message for if the user only chooses one integer
-            if (b == "")
+            if (b = "")
             {
-                double amount1 = a;
-                double amount2 = method.math(amount1);
+                int amount1 = a;
+                int amount2 = method.math(amount1);
                 Console.WriteLine("\nNo second integer found the result is:\n" +
                     a + " + " + 25 + " = " + amount2);
             }
             //If the user decides to enter two integers.
-            else if (b != null)
+            else if (b != "")
             {
-                double Amount3 = Double.Parse(b);
-                double amount = method.math(a, Amount3);
+                int Amount3 = (b);
+                int amount = method.math(a, Amount3);
                 Console.WriteLine("\n" + a + " + " + Amount3 + " + " + 25 + " = " + amount);
             }
 
